99b96cc9a2e775d4d4f080b2ce5ce100b8123ebd
Georgina Van Dort <GeorginaVanDort@gmail.com>
2016-06-10 09:46:50 -0700

    edit user interface JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,22 @@
 $(document).ready(function() {
-  $("#click").submit(function(event) {
-    var userInput = parseInt($("#input").val());
-    var numberList = pingPong(userInput);
-      $("#list").append("<li>" + numberList + "</li>")
+  $("#inputform").submit(function(event) {
+    event.preventDefault();
+      var userInput = parseInt($("#input").val());
+      var numberList = pingPong();
+        $("#list").append("<li>" + numberList + "</li>")
   });
 });
 
-for (var i= 1; i <= 100; i++) {
-  if (i % 15 === 0) {
-    console.log("pingpong")
-  }
-  if (i % 5 === 0) {
-    console.log("pong")
-  }
-  if (i % 3 === 0) {
-    console.log("ping")
+var pingPong = function(){
+  for (var i= 1; i <= 100; i++) {
+    if (i % 15 === 0) {
+      var output = "pingpong";
+    }
+    if (i % 5 === 0) {
+      var output = "pong";
+    }
+    if (i % 3 === 0) {
+      var output = "ping";
+    }
   }
 }
