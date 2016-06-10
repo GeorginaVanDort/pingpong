9906b7993cec6c6f632d8fd643224049aca2e7a2
Georgina Van Dort <GeorginaVanDort@gmail.com>
2016-06-10 09:50:21 -0700

    directs JS to display user input in UL

--- js/scripts.js
+++ js/scripts.js
@@ -2,21 +2,21 @@ $(document).ready(function() {
   $("#inputform").submit(function(event) {
     event.preventDefault();
       var userInput = parseInt($("#input").val());
-      var numberList = pingPong();
-        $("#list").append("<li>" + numberList + "</li>")
+      // var numberList = pingPong();
+        $("#list").append("<li>" + userInput + "</li>")
   });
 });
 
-var pingPong = function(){
-  for (var i= 1; i <= 100; i++) {
-    if (i % 15 === 0) {
-      var output = "pingpong";
-    }
-    if (i % 5 === 0) {
-      var output = "pong";
-    }
-    if (i % 3 === 0) {
-      var output = "ping";
-    }
-  }
-}
+// var pingPong = function(){
+//   for (var i= 1; i <= 100; i++) {
+//     if (i % 15 === 0) {
+//       var output = "pingpong";
+//     }
+//     if (i % 5 === 0) {
+//       var output = "pong";
+//     }
+//     if (i % 3 === 0) {
+//       var output = "ping";
+//     }
+//   }
+// }
