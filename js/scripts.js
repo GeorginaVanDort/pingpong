d5178961afdabbdff99e4d7b6865ce7d3f93263f
Georgina Van Dort <GeorginaVanDort@gmail.com>
2016-06-10 09:28:35 -0700

    Add working if statements

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,19 @@
 $(document).ready(function() {
   $("#click").submit(function(event) {
-    var userInput = $("#input").val();
-    var numberList = pingpong(userInput);
+    var userInput = parseInt($("#input").val());
+    var numberList = pingPong(userInput);
       $("#list").append("<li>" + numberList + "</li>")
-)
   });
 });
+
+for (var i= 1; i <= 100; i++) {
+  if (i % 15 === 0) {
+    console.log("pingpong")
+  }
+  if (i % 5 === 0) {
+    console.log("pong")
+  }
+  if (i % 3 === 0) {
+    console.log("ping")
+  }
+}
