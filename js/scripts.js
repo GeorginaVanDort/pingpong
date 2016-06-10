83532d82daba276efa774e1fea32686f1144b506
Georgina Van Dort <GeorginaVanDort@gmail.com>
2016-06-10 09:00:12 -0700

    Add basic files and specs

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,8 @@
 $(document).ready(function() {
-  $("#form").submit(function(event) {
-    event.preventDefault();
-    var snakes = $("input:radio[name=q1]:checked").val();
-    var temp = $("#q2").val();
-    var environment = $("#q3").val();
-    var fun = $("#q4").val();
-    var bevvy = $("#q5").val();
-
-    if (snakes === "yes") {
-      $(".result").hide();
-      $("#kakadu").show();
-    }
-
-      else if (snakes !== "no" && fun === "crocodile" || snakes !== "no" && temp !== "cold" && environment === "desert") {
-        $(".result").hide();
-        $("#kakadu").show();
-    }
-
-      else if (bevvy === "wine" || fun === "wine") {
-        $(".result").hide();
-        $("#barossa").show();
-    }
-
-      else if (temp !== "cold" && environment === "coast" || temp !== "cold" && fun === "surf") {
-        $(".result").hide();
-        $("#gold").show();
-    }
-
-      else if (temp !== "cold" && fun === "hike") {
-        $(".result").hide();
-        $("#daintree").show();
-    }
-
-      else if (temp === "cold" || environment === "urban" && fun === "dining") {
-        $(".result").hide();
-        $("#melbs").show();
-      }
-
-      else {
-        $(".result").hide();
-        $("#daintree").show();
-    }
-
+  $("#click").submit(function(event) {
+    var userInput = $("#input").val();
+    var numberList = pingpong(userInput);
+      $("#list").append("<li>" + numberList + "</li>")
+)
   });
 });
