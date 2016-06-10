471977325644a5ed62eaf1bc3e668cba2bb385e1
Georgina Van Dort <GeorginaVanDort@gmail.com>
2016-06-10 16:52:02 -0700

    tries and fails to change the core JS

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,6 @@
 // Business Logic//
 var pingPong = function(userInput){
 var userInput = parseInt($("#input").val());
-
   for (var i= 1; i <= userInput; i++) {
     if (i % 15 === 0) {
       $("#list").append("<li class='blink'>P I N G P 0 N G!</li>")
@@ -27,3 +26,34 @@ $(document).ready(function() {
     pingPong();
   });
 });
+
+
+
+// Here is where I was trying to separate out the "append list" and move it to User interface (but failed)
+// var pingPong = function(){
+// var userInput = parseInt($("#input").val());
+//   for (var i= 1; i <= userInput; i++) {
+//     if (i % 15 === 0) {
+//       result = "pingpong"
+//     }
+//     else if (i % 5 === 0) {
+//       result = "pong"
+//     }
+//     else if (i % 3 === 0) {
+//       result = "ping"
+//     }
+//     else {
+//       result = i
+//     }
+//   }
+//   return result;
+// }
+// ///user Interface//
+//
+// $(document).ready(function() {
+//   $("#inputform").submit(function(event) {
+//     event.preventDefault();
+//     $("#list").empty();
+//       pingPong().forEach($("#list").append("<li>" + result + "</li>"))
+//     });
+//   });
