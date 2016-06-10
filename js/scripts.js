a1dcb920380968882944a858b399abc3edbb3320
Georgina Van Dort <GeorginaVanDort@gmail.com>
2016-06-10 12:26:56 -0700

    style page with css

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
   $("#inputform").submit(function(event) {
     event.preventDefault();
+    $("#list").empty();
     pingPong();
+
   });
 });
 
@@ -10,13 +12,13 @@ var userInput = parseInt($("#input").val());
 
   for (var i= 1; i <= userInput; i++) {
     if (i % 15 === 0) {
-      $("#list").append("<li>pingpong</li>")
+      $("#list").append("<li class='blink'>P I N G P 0 N G!</li>")
     }
     else if (i % 5 === 0) {
-      $("#list").append("<li>pong</li>")
+      $("#list").append("<li class='blink'>P0NG!</li>")
     }
     else if (i % 3 === 0) {
-      $("#list").append("<li>ping</li>")
+      $("#list").append("<li class='blink'>PING!</li>")
     }
     else {
       $("#list").append("<li>" + i + "</li>")
