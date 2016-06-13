122428417923568f7c3e4ae197860e85a33f95d4
Georgina Van Dort <GeorginaVanDort@gmail.com>
2016-06-13 16:52:59 -0700

    change js code

--- js/scripts.js
+++ js/scripts.js
@@ -1,52 +1,21 @@
-
-// Business Logic//
-var pingPong = function(userInput){
-var userInput = parseInt($("#input").val());
-  for (var i= 1; i <= userInput; i++) {
-    if (i % 15 === 0) {
-      $("#list").append("<li class='blink'>P I N G P 0 N G!</li>")
-    }
-    else if (i % 5 === 0) {
-      $("#list").append("<li class='blink'>P0NG!</li>")
-    }
-    else if (i % 3 === 0) {
-      $("#list").append("<li class='blink'>PING!</li>")
-    }
-    else {
-      $("#list").append("<li>" + i + "</li>")
-    }
-  }
-}
-///user Interface//
-
-$(document).ready(function() {
-  $("#inputform").submit(function(event) {
-    event.preventDefault();
-    $("#list").empty();
-    pingPong();
-  });
-});
-
-
-
-// Here is where I was trying to separate out the "append list" instruction and move it to User interface (but failed).
-// var pingPong = function(){
+//
+// // Business Logic//
+// var pingPong = function(userInput){
 // var userInput = parseInt($("#input").val());
 //   for (var i= 1; i <= userInput; i++) {
 //     if (i % 15 === 0) {
-//       result = "pingpong"
+//       $("#list").append("<li class='blink'>P I N G P 0 N G!</li>")
 //     }
 //     else if (i % 5 === 0) {
-//       result = "pong"
+//       $("#list").append("<li class='blink'>P0NG!</li>")
 //     }
 //     else if (i % 3 === 0) {
-//       result = "ping"
+//       $("#list").append("<li class='blink'>PING!</li>")
 //     }
 //     else {
-//       result = i
+//       $("#list").append("<li>" + i + "</li>")
 //     }
 //   }
-//   return result;
 // }
 // ///user Interface//
 //
@@ -54,6 +23,42 @@ $(document).ready(function() {
 //   $("#inputform").submit(function(event) {
 //     event.preventDefault();
 //     $("#list").empty();
-//       pingPong().forEach($("#list").append("<li>" + result + "</li>"))
-//     });
+//     pingPong();
 //   });
+// });
+
+
+
+// Here is where I was trying to separate out the "append list" instruction and move it to User interface (but failed).
+var pingPong = function(){
+var userInput = parseInt($("#input").val());
+  var numArray = [];
+    for (var i= 1; i <= userInput; i++) {
+      if (i % 15 === 0) {
+        numArray.push("P I N G P 0 N G!")
+      }
+      else if (i % 5 === 0) {
+        numArray.push("P0NG!")
+      }
+      else if (i % 3 === 0) {
+        numArray.push("PING!")
+      }
+      else {
+        numArray.push(i)
+      }
+    }
+  return numArray;
+}
+///user Interface//
+
+$(document).ready(function() {
+  $("#inputform").submit(function(event) {
+    event.preventDefault();
+    $("#list").empty();
+    var numlist = pingPong();
+
+      numlist.forEach(function(entry){
+        $("#list").append("<li>" + entry + "</li>")
+      });
+    });
+  });
