85948f20c1ea4d547f3318355f946bad467cd63f
Georgina Van Dort <GeorginaVanDort@gmail.com>
2016-06-14 17:50:33 -0700

    divide business and user logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,14 @@
-//
-// // Business Logic//
-// var pingPong = function(userInput){
-// var userInput = parseInt($("#input").val());
-//   for (var i= 1; i <= userInput; i++) {
-//     if (i % 15 === 0) {
-//       $("#list").append("<li class='blink'>P I N G P 0 N G!</li>")
-//     }
-//     else if (i % 5 === 0) {
-//       $("#list").append("<li class='blink'>P0NG!</li>")
-//     }
-//     else if (i % 3 === 0) {
-//       $("#list").append("<li class='blink'>PING!</li>")
-//     }
-//     else {
-//       $("#list").append("<li>" + i + "</li>")
-//     }
-//   }
-// }
-// ///user Interface//
-//
-// $(document).ready(function() {
-//   $("#inputform").submit(function(event) {
-//     event.preventDefault();
-//     $("#list").empty();
-//     pingPong();
-//   });
-// });
-
-
-
-// Here is where I was trying to separate out the "append list" instruction and move it to User interface (but failed).
-var pingPong = function(){
-var userInput = parseInt($("#input").val());
+var pingPong = function(userInput){
   var numArray = [];
     for (var i= 1; i <= userInput; i++) {
       if (i % 15 === 0) {
-        numArray.push("P I N G P 0 N G!")
+            numArray.push("<p class='blink'>P I N G P 0 N G!</p>")
       }
       else if (i % 5 === 0) {
-        numArray.push("P0NG!")
+            numArray.push("<p class='blink'>P 0 N G!</p>")
       }
       else if (i % 3 === 0) {
-        numArray.push("PING!")
+            numArray.push("<p class='blink'>P I N G!</p>")
       }
       else {
         numArray.push(i)
@@ -54,11 +21,12 @@ var userInput = parseInt($("#input").val());
 $(document).ready(function() {
   $("#inputform").submit(function(event) {
     event.preventDefault();
-    $("#list").empty();
-    var numlist = pingPong();
+    var userInput = parseInt($("#input").val());
+      $("#list").empty();
+      var numArray = pingPong(userInput);
 
-      numlist.forEach(function(entry){
-        $("#list").append("<li>" + entry + "</li>")
+      numArray.forEach(function(entry){
+        $("#list").append("<li>" + entry + "</li>");
       });
     });
   });
